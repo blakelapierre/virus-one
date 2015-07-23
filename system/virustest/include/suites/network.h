614b01b9ab01205f973432ea762cccda23da0b93
Rob Colbert <rob.isConnected@gmail.com>
2015-07-23 05:07:08 -0400

    Added the "Network" test suite

---
+++ system/virustest/include/suites/network.h
@@ -0,0 +1,19 @@
+// suites/network.h
+// Copyright (C) 2015 Rob Colbert <rob.isConnected@gmail.com>
+// License: MIT (see LICENSE)
+
+#pragma once
+
+#include <virustest_suite.h>
+
+namespace VirusTest { namespace Suites {
+
+  class Network : public VirusTest::Suite {
+  public:
+
+    Network();
+    virtual ~Network();
+
+  };
+
+} /*Suites*/ } /*VirusTest*/
---
+++ system/virustest/src/suites/network.cpp
@@ -0,0 +1,12 @@
+// suites/network.cpp
+// Copyright (C) 2015 Rob Colbert <rob.isConnected@gmail.com>
+// License: MIT (see LICENSE)
+
+#include <suites/network.h>
+
+VirusTest::Suites::Network::Network()
+: VirusTest::Suite("Network")
+{}
+
+VirusTest::Suites::Network::~Network()
+{}
