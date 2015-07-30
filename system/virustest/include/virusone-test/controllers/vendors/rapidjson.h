5f2574590dcb5fff68015168241554248b56554e
Rob Colbert <rob.isConnected@gmail.com>
2015-07-30 05:03:55 -0400

    Quick fix to .gitignore for virusone-test

---
+++ system/virustest/include/virusone-test/controllers/vendors/rapidjson.h
@@ -0,0 +1,19 @@
+// controllers/vendor/rapidjson.h
+// Copyright (C) 2015 Rob Colbert <rob.isConnected@gmail.com>
+// License: MIT (see LICENSE)
+
+#pragma once
+
+#include <virustest/controller.h>
+
+namespace VirusTest { namespace Controllers { namespace Vendors {
+
+  class RapidJson : public VirusTest::Controller {
+  public:
+
+    RapidJson();
+    virtual ~RapidJson();
+
+  };
+
+} /* Vendor */ } /* Controllers */ } /* VirusTest */
